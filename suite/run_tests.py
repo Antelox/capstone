80e12b5bdb9569a8638399797cc196eae13aa7b0
Antelox <anteloxrce@gmail.com>
2024-11-13 14:34:28 +0100

    - Added SPDX headers to the run_tests.py script and to the build-wheels-publish.yml workflow file - Minor fixes to the workflow as pointed out in the PR review - Updated MANIFEST.in to reflect the actual libraries built during python wheel creation process - Use subprocess.run in place of os.system in run_tests.py script

--- suite/run_tests.py
+++ suite/run_tests.py
@@ -1,13 +1,17 @@
+# SPDX-FileCopyrightText: 2024 Antelox <anteloxrce@gmail.com>
+# SPDX-License-Identifier: BSD-3
+
 import logging
-import os
+import subprocess
+import sys
 from pathlib import Path
 
 logger = logging.getLogger('tests')
 logging.basicConfig(level=logging.INFO)
 root_dir = Path(__file__).parent.parent.resolve()
 tests = [
-    f"python {root_dir}/bindings/python/tests/test_all.py",
-    f"python {root_dir}/suite/cstest/test/integration_tests.py cstest_py",
+    f"{sys.executable} {root_dir}/bindings/python/tests/test_all.py",
+    f"{sys.executable} {root_dir}/suite/cstest/test/integration_tests.py cstest_py",
     f"cstest_py {root_dir}/tests/MC/",
     f"cstest_py {root_dir}/tests/details/",
     f"cstest_py {root_dir}/tests/issues/",
@@ -17,5 +21,5 @@ tests = [
 for test in tests:
     logger.info(f'Running {test}')
     logger.info("#######################")
-    os.system(test)
+    subprocess.run(test.split(" "), check=True)
     logger.info("-----------------------")
